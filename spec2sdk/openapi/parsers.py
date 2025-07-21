a54ed35388f3ac998bc578535d231fc72d2c874f
Evgeniy Krysanov <evgeniy.krysanov@gmail.com>
2025-07-21 16:06:08 +0300

    feat(spec2sdk): allow additionalProperties on object with properties

--- spec2sdk/openapi/parsers.py
+++ spec2sdk/openapi/parsers.py
@@ -131,11 +131,7 @@ def parse_null(schema: dict) -> NullDataType:
 
 @parsers.register(predicate=type_equals("object"))
 def parse_object(schema: dict) -> ObjectDataType:
-    additional_properties: bool = ("properties" not in schema) and schema.get("additionalProperties") in (
-        None,
-        True,
-        {},
-    )
+    additional_properties: bool = schema.get("additionalProperties") in (None, True, {})
 
     return ObjectDataType(
         **parse_common_fields(schema=schema),
